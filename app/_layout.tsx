02c520440085a04c5a80fe8add1bd454e676365b
Jeroen Peeters <contact@jeroenpeeters.be>
2024-10-10 22:00:16 +0200

    feat: add global styles

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,7 @@
-import { Provider } from "@/constants/messaging";
 import { Stack } from "expo-router";
+
+import "@/assets/css/index.css";
+import { Provider } from "@/constants/messaging";
 
 export default function RootLayout() {
 	return (
