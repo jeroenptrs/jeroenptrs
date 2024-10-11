7098c7ad202f40812404e4f2d94c8e47788e311f
Jeroen Peeters <contact@jeroenpeeters.be>
2024-10-11 22:42:41 +0200

    feat: add about me section

---
+++ game/resume/GB Studio Indoors BitsAndBobs.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="GB Studio Indoors BitsAndBobs" tilewidth="8" tileheight="8" tilecount="320" columns="16">
+ <image source="GB Studio Indoors BitsAndBobs.png" trans="65ff00" width="128" height="160"/>
+</tileset>
